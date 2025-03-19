7739e03c03c470b8edf91b936b9b37e72eb90c73
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-18 23:11:39 -0700

    Update player constants to enhance gameplay dynamics by increasing player body height and rotation speed.

--- src/constants.ts
+++ src/constants.ts
@@ -7,12 +7,12 @@ export const FIELD_LINE_THICKNESS = 0.12
 export const FIELD_LINE_COLOR = 0xffffff
 
 // Player
-export const PLAYER_BODY_HEIGHT = 1.7
+export const PLAYER_BODY_HEIGHT = 1.8
 export const PLAYER_BODY_RADIUS = 0.5
 export const PLAYER_COLOR = 0x0000ff
 export const PLAYER_SHININESS = 30
 export const PLAYER_MOVE_SPEED = 12
-export const PLAYER_ROTATION_SPEED = 3
+export const PLAYER_ROTATION_SPEED = 4
 export const PLAYER_JUMP_FORCE = 10
 export const PLAYER_GRAVITY = 20
 export const PLAYER_GROUND_LEVEL = 0.5
