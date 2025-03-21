7ca18d83b024eddc69f4d03a34506b207c343747
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-21 07:25:52 -0700

    Refactor overlay implementation to streamline score display and improve element handling

--- src/overlay.ts
+++ src/overlay.ts
@@ -1,8 +1,26 @@
-fetch('vibes-fc/overlay/score.html')
-    .then((response) => response.text())
-    .then((html) => {
-        const scoreOverlay = document.getElementById('score-overlay')
-        if (scoreOverlay) {
-            scoreOverlay.innerHTML = html
+export class Overlay {
+    overlayElement: HTMLElement
+
+    constructor() {
+        this.overlayElement = document.getElementById('overlay') as HTMLElement
+
+        if (!this.overlayElement) {
+            console.error('Overlay element not found!')
+            return
         }
-    })
+
+        this.createScore()
+    }
+
+    createScore() {
+        fetch('vibes-fc/overlay/score.html')
+            .then((response) => response.text())
+            .then((html) => {
+                const scoreElement = document.createElement('div')
+                scoreElement.innerHTML = html
+                this.overlayElement.appendChild(scoreElement)
+            })
+    }
+}
+
+new Overlay()
